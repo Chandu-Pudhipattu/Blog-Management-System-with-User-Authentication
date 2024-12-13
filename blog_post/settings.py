14596ad16c3cd142bf95a351eaabede4fdf21d7a
Chandu <chanducn3109@gmail.com>
2024-12-12 19:37:25 -0600

    Updated Databases

--- blog_post/settings.py
+++ blog_post/settings.py
@@ -83,8 +83,7 @@ WSGI_APPLICATION = 'blog_post.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
     }
 }
 
